e4c4804fe6f99d83b7448c1b8a3174756719c2b9
KALYAN GUPTA <kalyangupta2002@gmail.com>
2025-02-23 00:55:45 +0530

    Recursion functional and Param problem

---
+++ src/ParamFuncRecursion.java
@@ -0,0 +1,26 @@
+public class ParamFuncRecursion {
+    public static void ParamNAdd(int n, int sum){
+        if(n<1) {
+            System.out.println(sum);
+            return;
+        }
+        ParamNAdd(n-1, sum+n);
+    }
+    public static int FuncNAdd(int n){
+        if(n==0) return 0;
+        return (n + FuncNAdd(n-1));
+    }
+    //Time and space complexity will be O(n)
+    //n times stack space occupied
+    public static int FuncNFact(int n){
+        if(n==1) return 1;
+        return (n * FuncNFact(n-1));
+    }
+    public static void main(String[] args) {
+        //Parameterised Recursion
+        ParamNAdd(3, 0);
+        //Functional Recursion
+        System.out.println("The Addition for this will be " + FuncNAdd(3));
+        System.out.println("The Factorial for this will be " + FuncNFact(5));
+    }
+}
